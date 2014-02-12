7d7683efd99bcb68436c72d5448e998cd6197a4d
ollehallman <olle.hallman@gmail.com>
2014-02-13 00:32:58 +0100

    Create basic MVC app with user access control

---
+++ InternetApp/Controllers/ProjectController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InternetApp.Models;
+using System.Web.Security;
+using WebMatrix.WebData;
+using InternetApp.Filters;
+
+namespace InternetApp.Controllers
+{
+    [Authorize]
+    [InitializeSimpleMembership]
+    public class ProjectController : Controller
+    {
+        private ProjectsContext db = new ProjectsContext();
+
+        //
+        // GET: /Project/
+        public ActionResult Index()
+        {
+            var memberId = WebSecurity.GetUserId(User.Identity.Name);
+            ViewBag.memberIdent = memberId;
+
+            return View(db.Projects.ToList());
+        }
+
+        //
+        // GET: /Project/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        //
+        // GET: /Project/Create
+
+        public ActionResult Create()
+        {
+            var memberId = WebSecurity.GetUserId(User.Identity.Name);
+            ViewBag.memberIdent = memberId;
+
+            return View();
+        }
+
+        //
+        // POST: /Project/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Projects.Add(project);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(project);
+        }
+
+        //
+        // GET: /Project/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            var memberId = WebSecurity.GetUserId(User.Identity.Name);
+
+            using (ProjectsContext db = new ProjectsContext()) {
+                int projUsrId = (from p in db.Projects
+                                 where p.ProjectId == id
+                                 select p.UserId).Single();
+
+                 Project project = db.Projects.Find(id);
+                 if (project == null)
+                {
+                    return HttpNotFound();
+                } else if (memberId == projUsrId) {
+                    return View(project);
+                }
+                 else
+                {
+                return RedirectToAction("Index");
+                }
+            }
+        }
+
+        //
+        // POST: /Project/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Project project)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(project).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(project);
+        }
+
+        //
+        // GET: /Project/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            var memberId = WebSecurity.GetUserId(User.Identity.Name);
+
+            using (ProjectsContext db = new ProjectsContext())
+            {
+                int projUsrId = (from p in db.Projects
+                                 where p.ProjectId == id
+                                 select p.UserId).Single();
+
+                Project project = db.Projects.Find(id);
+                if (project == null)
+                {
+                    return HttpNotFound();
+                }
+                else if (memberId == projUsrId)
+                {
+                    return View(project);
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+        }
+
+        //
+        // POST: /Project/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+
+            Project project = db.Projects.Find(id);
+            db.Projects.Remove(project);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ InternetApp/Models/ProjectModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace InternetApp.Models
+{
+    public class ProjectsContext : DbContext
+    {
+        public ProjectsContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+
+    [Table("Project")]
+    public class Project
+    {
+        public string Name { get; set; }
+        public int Difficulty { get; set; }
+        public string Detail { get; set; }
+        [Key]
+        public int ProjectId { get; set; }
+        public decimal Price { get; set; }
+        public int UserId { get; set;}
+        public string Skill { get; set; }
+    }
+}
